ae9b2e89b2083340568a684bca25698cc2634022
lxc <1215995953@qq.com>
2021-03-23 16:26:32 +0800

    把本地工程添加到git

---
+++ src/com/itheima/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+/**
+ * @author ASUS
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+}
